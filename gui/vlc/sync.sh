5013c70eff16e3a54ff49fc6e6fb6607cd34631e
bradleyoesch <bradley.oesch@gmail.com>
2026-02-17 14:15:45 -0600

    fix missed file rename

--- gui/vlc/sync.sh
+++ gui/vlc/sync.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "import" ]; then
     echo "Importing settings to VLC from version control..."
-    cp -f applications/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc
+    cp -f gui/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc
 elif [ "$1" = "export" ]; then
     echo "Exporting settings from VLC to version control..."
-    cp -f ~/Library/Preferences/org.videolan.vlc/vlcrc applications/vlc/vlcrc
+    cp -f ~/Library/Preferences/org.videolan.vlc/vlcrc gui/vlc/vlcrc
 else
     echo "Invalid argument \"$1\", must be either \"import\" or \"export\""
 fi
